135d48a00811bd09fcda1ab8f8b9a7eb3454204d
ngwenyatm <thabsnspaces@gmail.com>
2024-08-07 20:29:01 +0200

    pushed with script

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
-function getFullResponseFromAPI(success){
+function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-        if (success){
-            resolve({
-        status:200,
-        body:'Success'})
-    }
-    else{
-    reject(new Error('The fake API is not working currently'))
-}});}
-
-export default getFullResponseFromAPI;
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
+  export default getFullResponseFromAPI;
