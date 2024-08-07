9a0acd68ac3d54ef959fe7c2e20e233f94923c0c
ngwenyatm <thabsnspaces@gmail.com>
2024-08-07 21:20:36 +0200

    pushed with script

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,3 @@
 export default  function handleResponseFromAPI(promise) {
-    return promise.then(response => {
-        return {
-          status: 200,
-          body: 'Success'
-        };
-      }).catch(error => {
-        console.log('Got a response from the API');
-        return new Error('');
-      });
-  }
+    return promise.then({status:200, body: 'Success'}).catch(new Error(''))
+    .finally(console.log('Got a response from the API'))}
