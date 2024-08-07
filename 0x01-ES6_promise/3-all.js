00fc4ca75dd70e1e25d98c7e500944e380296dc0
ngwenyatm <thabsnspaces@gmail.com>
2024-08-07 23:11:47 +0200

    pushed with script

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-return Promise.all([uploadPhoto(), createUser()]).then((responses) => {
-const [photoResponse, userResponse] = responses;
-console.log(`${photoResponse.body} ${userResponse.firstName}
-${userResponse.lastName}`);
-}).catch(() => {
-console.log('Signup system offline');
-});
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [user] = results;
+      console.log(`${user.body} ${user.firstName} ${user.lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
 }
