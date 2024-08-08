53ea135ff42b7507809173e3ae192a488ee58066
ngwenyatm <thabsnspaces@gmail.com>
2024-08-08 18:50:44 +0200

    pushed with script

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,62 @@
+import { array, number } from "yargs";
+
+export default class HolbertonCourse{
+    constructor(name,length,students){
+        if (typeof(name) === string){
+            this._name = name;
+        }
+        else throw new TypeError('name must be a string');
+
+        if (typeof(length) === number){
+            this._length = length;
+        }
+        else throw new TypeError('length must be a number');
+
+        if (Array.isArray(students)) {
+            this._students = students;
+        }
+        else throw new TypeError('Length must be a number');
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        if (typeof(name) === 'string'){
+            this._name = name;
+        }
+        else throw new TypeError('name must be a string')
+    }
+
+    get length(){
+        return this._length;
+
+    }
+    set length(length){
+
+        if (typeof(length) !== 'number') {
+            throw new TypeError('Length must be a number');
+          }
+      
+          this._length = length;
+
+    }
+
+    get students(){
+        this._students;
+
+    }
+
+    set students(){
+        if (
+            !Array.isArray(students) &&
+            !students.every(typeof students === 'string')
+          ) {
+            throw new TypeError('Students must be an array of string');
+          }
+      
+          this._students = students;
+
+    })
+}
