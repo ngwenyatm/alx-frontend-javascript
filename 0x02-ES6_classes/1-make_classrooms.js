8810f11d708ef457bb55ee62c988bae8810d31f3
ngwenyatm <thabsnspaces@gmail.com>
2024-08-08 18:10:07 +0200

    pushed with script

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import { array } from 'yargs';
+import ClassRoom from './0-classroom';
+
+function initializeRooms(){
+    const ClassRoomArr = [new ClassRoom(19),new ClassRoom(20),new ClassRoom(34)];
+    return ClassRoomArr
+}
