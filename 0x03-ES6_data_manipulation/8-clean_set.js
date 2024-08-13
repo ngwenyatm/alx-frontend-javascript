75ff515d9a4098edfd9b03f4e4d4104e6a60da40
ngwenyatm <thabsnspaces@gmail.com>
2024-08-14 00:17:24 +0200

    pushed with script

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(inputSet, prefix) {
+  const cleanedStrings = [];
+
+  if (prefix === '' || typeof prefix !== 'string') return '';
+
+  inputSet.forEach(function (item) {
+    if (typeof item === 'string' && item.startsWith(prefix)) {
+      cleanedStrings.push(item.slice(prefix.length));
+    }
+  });
+
+  return cleanedStrings.join('-');
+}
+
