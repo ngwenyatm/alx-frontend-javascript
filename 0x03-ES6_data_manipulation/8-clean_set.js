340ad5ccd8c1cc9e83ee610d61fe230dd871a642
ngwenyatm <thabsnspaces@gmail.com>
2024-08-14 00:22:37 +0200

    pushed with script

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,12 +3,11 @@ export default function cleanSet(inputSet, prefix) {
 
   if (prefix === '' || typeof prefix !== 'string') return '';
 
-  inputSet.forEach(function pushString (item) {
-    if (typeof item === 'string' && item.startsWith(prefix)) {
-      cleanedStrings.push(item.slice(prefix.length));
-    }
-  });
+  inputSet.forEach((item) => {
+  if (typeof item === 'string' && item.startsWith(prefix)) {
+    cleanedStrings.push(item.slice(prefix.length));
+  }
+});
 
   return cleanedStrings.join('-');
 }
-
