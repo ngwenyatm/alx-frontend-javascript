a1d45312a60f69c610b5a3674f2f2faf05f92106
ngwenyatm <thabsnspaces@gmail.com>
2024-08-13 21:56:06 +0200

    pushed with script

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,8 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error("Position outside range");
   }
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
-  return buffer;
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+
+  return int8Array;
 }
