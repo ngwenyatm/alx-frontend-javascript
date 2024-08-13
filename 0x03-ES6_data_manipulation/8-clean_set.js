b28edc3f2d2f609eb130401bf0b71ea70297c1bd
ngwenyatm <thabsnspaces@gmail.com>
2024-08-14 00:25:52 +0200

    pushed with script

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@ export default function cleanSet(inputSet, prefix) {
   const cleanedStrings = [];
   if (prefix === '' || typeof prefix !== 'string') return '';
   inputSet.forEach((item) => {
-  if (typeof item === 'string' && item.startsWith(prefix)) {
-  cleanedStrings.push(item.slice(prefix.length));
+    if (typeof item === 'string' && item.startsWith(prefix)) {
+      cleanedStrings.push(item.slice(prefix.length));
   }
-});
+  });
   return cleanedStrings.join('-');
 }
