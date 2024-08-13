6671df7991d1d232a37c92bb0c384819a6b08eef
ngwenyatm <thabsnspaces@gmail.com>
2024-08-13 23:21:08 +0200

    pushed with script

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation (list, city){
+export default function getStudentsByLocation(list, city) {
     const result = list.filter((city) => list.location === city);
+
+    return result;
 }
