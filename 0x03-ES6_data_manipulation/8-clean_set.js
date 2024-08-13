3fe810020cfeae7e7c69b70dee785e438bc6f27f
ngwenyatm <thabsnspaces@gmail.com>
2024-08-14 00:19:59 +0200

    pushed with script

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(inputSet, prefix) {
 
   if (prefix === '' || typeof prefix !== 'string') return '';
 
-  inputSet.forEach(function (item) {
+  inputSet.forEach(function pushString (item) {
     if (typeof item === 'string' && item.startsWith(prefix)) {
       cleanedStrings.push(item.slice(prefix.length));
     }
